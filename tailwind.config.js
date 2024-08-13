fe9ec96980f02f054a1f42d7ce5639d5c2efc3c1
Andres <andres.marin.abad+git@gmail.com>
2024-08-13 17:22:52 +0200

    Build tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './public/static/**/*.js',  // Incluye tus archivos JS si usas Tailwind en ellos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
